d6a092d4b1fdc7af678731f1e013e452f167fd5d
LillaKoncz <koncz.lilla92@gmail.com>
2023-09-24 20:49:20 +0200

    import naav to app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {About} from './components/about/About'
 import {Home} from './components/home/Home'
 import {Works} from './components/works/Works'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Nav} from './components/nav/Nav'
 
 
 
