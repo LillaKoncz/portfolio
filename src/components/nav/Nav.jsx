c0b0c515c3bef20d6c82022eca7ed4eb9fcf273d
LillaKoncz <koncz.lilla92@gmail.com>
2023-09-24 20:53:36 +0200

    toggle removed

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,31 +1,17 @@
-import {React, useState} from 'react'
+import React from 'react'
 import './nav.css'
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 export const Nav = () => {
-  const [isOpen, setIsOpen]=useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  }
-
   return (
     <>
-    <nav className={`nav ${isOpen ? 'open' : ''}`}> 
+    <nav className='nav'> 
         <ul className='nav-list'>
             <li className='nav-item'><AnchorLink href='#about' className='navlink'>About</AnchorLink></li>
             <li className='nav-item'><AnchorLink href='#works' className='navlink'>Works</AnchorLink></li>
             <li className='nav-item'><AnchorLink href='#contact'className='navlink'>Contact</AnchorLink></li>
         </ul>
     </nav>
-
-
-
-      <div className={`burger-menu ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
-          <div className="bar"></div>
-          <div className="bar"></div>
-          <div className="bar"></div>
-      </div>
     </>
   )
 }
