46f29acdc7af5b7b00c7a9f445bb1f685e9bd2f0
LillaKoncz <koncz.lilla92@gmail.com>
2023-09-24 22:00:26 +0200

    useState in Nav and functions

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
+import {React, useState} from 'react'
 import './nav.css'
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 export const Nav = () => {
+  const [isOpen, setIsOpen]=useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  }
+
+
   return (
     <>
-    <nav className='nav'> 
+     <nav className={`nav ${isOpen ? 'open' : ''}`}> 
         <ul className='nav-list'>
             <li className='nav-item'><AnchorLink href='#about' className='navlink'>About</AnchorLink></li>
             <li className='nav-item'><AnchorLink href='#works' className='navlink'>Works</AnchorLink></li>
             <li className='nav-item'><AnchorLink href='#contact'className='navlink'>Contact</AnchorLink></li>
         </ul>
     </nav>
+
+
+
+      <div className={`burger-menu ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+      </div>
+
     </>
   )
 }
